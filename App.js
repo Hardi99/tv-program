915ec888d49d109c37f530723df58dbca845d39a
Hardi99 <harditabuna@gmail.com>
2023-09-13 17:21:58 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import data from './assets/data.json'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {data.map((program, index) => {
+        return (
+          <View key={index} style={styles.bloc}>
+            <Text style={[styles.bold, styles.head]}>{program.time}</Text>
+            <Image
+              source={{
+                uri: program.image
+              }}
+              style={styles.logo}
+              resizeMode="contain"
+              />
+            <View>
+              <Text style={[styles.bold]}>{program.title}</Text>
+              <Text>{program.type}</Text>
+              <View style={styles.finalInfo}>
+                <Text style={styles.bold}>{program.duration}</Text>
+                {program.direct === true ? <Text style={[styles.bold, styles.red]}>En direct</Text> : null}
+                {program.isUnseen === true ? <Text style={[styles.bold, styles.red]}>Inédit</Text> : null}
+              </View>
+            </View>
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  finalInfo: {
+    flexDirection: 'row',
+    gap: 15
+  },
+  bloc: { 
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+    borderBottomColor: 'grey',
+    margin: 10
+  },
+  head : {
+    justifyContent: 'center'
+  },
+  cover: { height: 300, width: 200, marginTop: 100 },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    height: 80,
+    width: 80,
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+  red: {
+    color: 'red'
+  }
 });
